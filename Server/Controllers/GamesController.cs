1b195c9b318e5b622105414452696e8832c50bf4
galcor11 <galgrossman2@gmail.com>
2026-05-03 11:03:30 +0300

    ADDDPREPOSITORY

--- Server/Controllers/GamesController.cs
+++ Server/Controllers/GamesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UsersManager.Server;
@@ -8,9 +9,14 @@ namespace AuthTemplate.Server.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [ServiceFilter(typeof(AuthCheck))] //בדיקה שהמשתמש מחובר
-
     public class GamesController : ControllerBase
     {
+        private readonly DbRepository _db;
+        public GamesController( DbRepository db) 
+        { 
+            _db = db;
+            // בתוך הקיימות השיטות אל לפנות מאפשר DBRepository
+        }
         [HttpGet]
         public async Task<ActionResult<int>> GetLoginUser(int authUserId)
         {
